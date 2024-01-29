5de15a7be5ee0468b3851d34927346faa8869e40
Nicolas Solerieu <nsolerieu@github.com>
2024-01-29 11:38:34 -0800

    Create 404 page

---
+++ www/404.php
@@ -0,0 +1,26 @@
+<?php $page = "404";?>
+<?php include "header.php";?>
+
+<main>
+
+  <section class="hero" >
+    <div class="container">
+      <div class="row">
+        <div class="col-100">
+          <h1 class="text-color-primary no-margin">Bummer</h1>
+          <p class="small-margin-top text-regular text-color-secondary">
+            I'm sorry you got there, kind soul. We all get there somedays. Sometimes one need to let the cycle run. From one breath, one tab switch to one sh*t day. Let it be. Stay here for a second. Then get back in there.
+          </p>
+          <p class="medium-margin-top text-regular text-color-primary">
+            "All that is gold does not glitter,<br>
+            Not all those who wander are lost"<br>
+            <span class="text-small text-color-secondary">J.R.R. Tolkien</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+</main>
+
+<?php include "footer.php";?>
