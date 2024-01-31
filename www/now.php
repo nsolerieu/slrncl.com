d7d300706619b1fa145a8996271b855f346eb316
Nicolas Solerieu <nsolerieu@github.com>
2024-01-31 08:28:22 -0800

    Update now.php

--- www/now.php
+++ www/now.php
@@ -21,29 +21,21 @@
     <div class="container">
       <div class="row">
         <div class="col-100">
-          <h2 class="text-color-primary no-margin">Life</h2>
+          <h2 class="text-color-primary no-margin">Settling in SLO</h2>
           <p class="small-margin-top text-regular text-color-secondary">
-            My first priority is to slow down - quite literally. Generally doing less and enjoying more. I'm a monkey with a tech job and a growing family. I care a lot about very few things (a recent realization). That means commitment to just a few people, interests, and projects. That makes life feel simple(r) and sincere. 
+            We finally made it to San Luis Obispo. The busyness of the move is tapering off allowing opportunities for moments of peace. Get out around the lake. Doing less and enjoying more. I'm still a monkey with a tech job and a growing family. The house is a bit empty, we don't know anyone around, still no car. Few things to figure out.
           </p>
           <h2 class="text-color-primary large-margin-top">Work</h2>
           <p class="small-margin-top text-regular text-color-secondary">
             These days most of my productive design time goes to GitHub. The rest of my limited availability is occasionally invested in shorter (cherry picked) engagements. If my stuff resonates with you and you have ideas, feel free to <a href="index#links">reach out</a>. 
           </p>
-          <h2 class="text-color-primary large-margin-top">Design therapy</h2>
-          <p class="small-margin-top text-regular text-color-secondary">
-            I don't do Design Therapy anymore. It's truly been therapeutic. I made some lovely connections. You can still send me an <a href="index#links">email</a>, I like to read them and I reply to most.
-          </p>
-          <h2 class="text-color-primary large-margin-top">Seeking...</h2>
+          <h2 class="text-color-primary large-margin-top">(still) Seeking...</h2>
           <p class="small-margin-top text-regular text-color-secondary">
             I've been struggling with becoming a dad. Fatigue is getting the best of me and I can't put on any weight or really pay my sleep debt at the moment. This transition is pushing me to acknowledge the "qualitative" aspect of life. At the heart of all the books, anecdotes or conversation on the matter, the good life seems to be a sort of dynamic contemplation with moments of deep appreciation.
           </p>
-          <h2 class="text-color-primary large-margin-top">Not writing</h2>
+          <h2 class="text-color-primary large-margin-top">blogging?</h2>
           <p class="small-margin-top text-regular text-color-secondary">
-            I have a complicated relationship with this website. I've been redesigning it too often but love to tinker on it. So far I'm denying myself the right to have a blog. I have my <a href="index#footnotes">footnotes</a> if I want to blab. It's on my homepage and some of the stuff in there is really personal and silly. 
-          </p>
-          <h2 class="text-color-primary large-margin-top">2023 recap</h2>
-          <p class="small-margin-top text-regular text-color-secondary">
-            Lot has happened this year –– 2023: The agony, and ecstasy. I am working on my end year note, mostly to get it out of my head. Baby, work shit, existential thought, meltdowns and feelings of belonging, the whole thing in words.
+            I'm quietly bringing my blog back. It's not in the primary nav yet as I'm figuring out the setup and content. This might take a while to take a decent shape and end up be a bit messy and personal (in a therapeutic way). 
           </p>
           <h2 class="text-color-primary large-margin-top">Making a (video) reel</h2>
           <p class="small-margin-top text-regular text-color-secondary">
