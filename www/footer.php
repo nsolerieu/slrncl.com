35676572f99ad9c3fc7cb8ffe2cfda230c3ee7f6
Nicolas Solerieu <nsolerieu@github.com>
2024-03-27 20:35:59 -0700

    Custom footer columns

--- www/footer.php
+++ www/footer.php
@@ -1,35 +1,35 @@
 <footer class="large-padding-top large-padding-bottom">
   <div class="container">
-    
     <div class="row">
-      <div class="col-50">
-        <h2>Information</h2>
-        <ul>
-          <li><a href="<?php echo BASE_URL; ?>/about">About</a></li>
-          <li><a href="<?php echo BASE_URL; ?>/work">Work</a></li>
-          <li><a href="<?php echo BASE_URL; ?>/now">Now</a></li>
-          <li><a href="<?php echo BASE_URL; ?>/contact">Contact</a></li>
-          <li><a href="<?php echo BASE_URL; ?>/colophon">Colophon</a></li>
-        </ul>
-      </div>
-      <div class="col-50">
-        <h2>Browse</h2>
-        <ul>
-          <li><a href="<?php echo BASE_URL; ?>/blog/">Blog</a></li>
-          <li><a href="<?php echo BASE_URL; ?>/moments/">Moments</a></li>
-          <li><a href="<?php echo BASE_URL; ?>/notes">Notes</a></li>
-          <li><a href="<?php echo BASE_URL; ?>/things">Things</a></li>
-          <li><a href="<?php echo BASE_URL; ?>/bikes">Bikes</a></li>
-        </ul>
-      </div>
-    </div>
-
-    <div class="row large-margin-top">
       <div class="col-100">
+    
+        <div class="columns">
+          <div class="column">
+            <h2>Information</h2>
+            <ul>
+              <li><a href="<?php echo BASE_URL; ?>/about">About</a></li>
+              <li><a href="<?php echo BASE_URL; ?>/work">Work</a></li>
+              <li><a href="<?php echo BASE_URL; ?>/now">Now</a></li>
+              <li><a href="<?php echo BASE_URL; ?>/contact">Contact</a></li>
+              <li><a href="<?php echo BASE_URL; ?>/colophon">Colophon</a></li>
+            </ul>
+          </div>
+          <div class="column">
+            <h2>Browse</h2>
+            <ul>
+              <li><a href="<?php echo BASE_URL; ?>/blog/">Blog</a></li>
+              <li><a href="<?php echo BASE_URL; ?>/moments/">Moments</a></li>
+              <li><a href="<?php echo BASE_URL; ?>/notes">Notes</a></li>
+              <li><a href="<?php echo BASE_URL; ?>/things">Things</a></li>
+              <li><a href="<?php echo BASE_URL; ?>/bikes">Bikes</a></li>
+            </ul>
+          </div>
+        </div>
+
         <div class="avatar"></div>
+        
       </div>
     </div>
-
   </div>
 </footer>
 
