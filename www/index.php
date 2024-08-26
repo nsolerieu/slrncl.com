122ec8b3683c818783f8b0e8abc4a9231ee0554e
Nicolas Solerieu <nsolerieu@github.com>
2024-08-26 11:32:21 -0700

    Fix typo - thanks loane!

--- www/index.php
+++ www/index.php
@@ -22,7 +22,7 @@
         <div class="col-100">
           <h2>Welcome</h2>
           <p class="medium-margin-top text-color-secondary">
-            I’m a designer, cyclist, and odd dad living in San Luis Obispo, CA. I'm currently working at <a href="https://github.com/nsolerieu">GitHub</a> as a web designer. Before that I was at <a href="https://www.opendoor.com/">Opendoor</a> and aside of my main gig I've been working with early stage startup, friends, and my own proejcts. This is my humble website, may you wish to discover what I'm up to, you will be able to answer this by exploring the links below.
+            I’m a designer, cyclist, and odd dad living in San Luis Obispo, CA. I'm currently working at <a href="https://github.com/nsolerieu">GitHub</a> as a web designer. Before that I was at <a href="https://www.opendoor.com/">Opendoor</a> and aside of my main gig I've been working with early stage startup, friends, and my own projects. This is my humble website, may you wish to discover what I'm up to, you will be able to answer this by exploring the links below.
           </p>
         </div>
       </div>
