6bbbd235ad9f626f38dc84aeb902606801e4be1d
Nicolas Solerieu <nsolerieu@github.com>
2024-09-24 14:05:15 -0700

    Update now.php

--- www/now.php
+++ www/now.php
@@ -9,7 +9,7 @@
         <div class="col-100">
           <h1>Now</h1>
           <p class="medium-margin-top text-small text-color-secondary max-width-400">
-             Updated Jun 7th, 2024
+             Updated Sept 24th, 2024
           </p>
         </div>
       </div>
@@ -20,21 +20,25 @@
     <div class="container">
       <div class="row full-row-border-top">
         <div class="col-100">
-          <h2>Deloading</h2>
-          <p class="medium-margin-top text-color-secondary">
-            I'm barely scraping by between work and familly life. The next couple of month will be emotionally intense and will require me to reduce my workload and learn to chill. I don't know how to have fun, that's fine but my inability to rest is starting to be problem. I'll be using this site as a coping tool and a wayt to feed the soul while not technically be pressured to deliver anything (it doesn't feel like work so far).
+          <h2>OOO</h2>
+          <p class="small-margin-top text-color-secondary">
+            I'm about to go on a second parental leave. One that I will finally use properly. I will not do ANY work until Feb 2025.
           </p>
-          <h2 class="large-margin-top">Blogging</h2>
-          <p class="medium-margin-top text-color-secondary">
-            I'm having alot of fun jamming and posting. That's my only current side project and it feels great. So far it's relatively silly. First assessment is coming.
+          <h2 class="large-margin-top">Planning</h2>
+          <p class="small-margin-top text-color-secondary">
+            I'm increasingly convinced I won't last much longer in big tech. I've only ever been a web designer, I like it and can't seriously think of anything else I would do. Especially when paying a mortgage in California. I don't have and don't want a great big plan but I'll be engaging in something I don't usually don't: planning. I'm unsure what it means yet but I must have a greater sense of purpose than "keep going" when I get back to work next year.
           </p>
-          <h2 class="large-margin-top">(professional) Social media?</h2>
-          <p class="medium-margin-top text-color-secondary">
-            I'm re-assessing my relationship with dribbble, read.cv, layer.to. I like contributing to the design community but the anxiety and weirdness of it feels like it's just another shade of social media... 
+          <h2 class="large-margin-top">Self therapy</h2>
+          <p class="small-margin-top text-color-secondary">
+            Posting random stories on my blog has been fun and revealing. The comedic relief is worth the time but the content looks a bit dubious. Some positive connections and feeback came out of it. It seems to be one of these things I instantly feel silly for but it will likely be an acceptable memory in the long run. I'm in a weird moment of life. 
           </p>
-          <h2 class="text-color-primary large-margin-top">Sketches</h2>
-          <p class="medium-margin-top text-color-secondary">
-            I'm trying to stop being a compulsive doodler. I blame the ipad. When I make something decent, I sometimes post it <a href="https://folio.procreate.com/slrncl">Here</a>.
+          <h2 class="large-margin-top">Co-sleeping</h2>
+          <p class="small-margin-top text-color-secondary">
+            It has been a few months. Some good, some bad. That's the kind of experience that makes me feel emotionally older. It stings harder. I have more range and resilience. I'll likely stick with it as long as I can or he wants it. I'm in bed at 8:30, some days I hate it, most days I'm indifferent. He almost broke my nose once. It has to go somewhere.
+          </p>
+          <h2 class="text-color-primary large-margin-top">Posters</h2>
+          <p class="small-margin-top text-color-secondary">
+            I'm (still) all over the place feeding the feeds, even in my low-social media ecosystem. Low, not no... dribbble, read.cv, layers.to, behance still tickles me, on top of all the dozen ideas that pop up every day. So instead of posting our there, I'll try to create a personal outlet in the form of a gallery/folder as I did for my photography with the <a href="https://slrncl.com/moments/">/moments</a> page.
           </p>
           <img src="assets/img/drawing-slrncl.jpg" class="fluid-img medium-margin-top" alt="portrait of a tall, lanky man sitting cross legged from the back">
         </div>
