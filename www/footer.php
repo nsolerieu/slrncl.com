3beb6539a4f3b381257e93299f0e9a67fedcf09e
Nicolas Solerieu <nsolerieu@github.com>
2025-01-20 06:41:30 -0800

    Footer rebuild with new grid

--- www/footer.php
+++ www/footer.php
@@ -1,36 +1,31 @@
 <footer class="large-padding-top large-padding-bottom">
   <div class="container">
     <div class="row full-row-border-top">
-      <div class="col-100">
-    
-        <div class="nav-columns large-margin-bottom">
-          <div class="column large-margin-top">
-            <ul>
-              <li><a href="<?php echo BASE_URL; ?>/about">About</a></li>
-              <li><a href="<?php echo BASE_URL; ?>/work">Work</a></li>
-              <li><a href="<?php echo BASE_URL; ?>/blog/">Blog</a></li>
-              <li><a href="<?php echo BASE_URL; ?>/now">Now</a></li>
-              <li><a href="<?php echo BASE_URL; ?>/contact">Contact</a></li>
-            </ul>
-          </div>
-          <div class="column large-margin-top">
-            <ul>
-              <li><a href="<?php echo BASE_URL; ?>/moments/">Moments</a></li>
-              <li><a href="<?php echo BASE_URL; ?>/notes">Notes</a></li>
-              <li><a href="<?php echo BASE_URL; ?>/sketchbook">Sketchbook</a></li>
-              <li><a href="<?php echo BASE_URL; ?>/things">Things</a></li>
-              <li><a href="<?php echo BASE_URL; ?>/bikes">Bikes</a></li>
-            </ul>
-          </div>
-        </div>
-
-        <div class="footer-bottom">
-          <div class="avatar"></div>
-          <a href="<?php echo BASE_URL; ?>/colophon" class="text-small text-color-secondary">Colophon</a>
-          <span>&bull;</span>
-          <a href="mailto:nico@slrncl.com" class="text-small text-color-secondary">Email</a>
-        </div>
-        
+      <div class="col-50 dont-collapse large-padding-bottom large-padding-top">
+        <ul>
+          <li><a href="<?php echo BASE_URL; ?>/about">About</a></li>
+          <li><a href="<?php echo BASE_URL; ?>/work">Work</a></li>
+          <li><a href="<?php echo BASE_URL; ?>/blog/">Blog</a></li>
+          <li><a href="<?php echo BASE_URL; ?>/now">Now</a></li>
+          <li><a href="<?php echo BASE_URL; ?>/contact">Contact</a></li>
+        </ul>
+      </div>
+      <div class="col-50 dont-collapse large-padding-bottom large-padding-top">
+        <ul>
+          <li><a href="<?php echo BASE_URL; ?>/moments/">Moments</a></li>
+          <li><a href="<?php echo BASE_URL; ?>/notes">Notes</a></li>
+          <li><a href="<?php echo BASE_URL; ?>/sketchbook">Sketchbook</a></li>
+          <li><a href="<?php echo BASE_URL; ?>/things">Things</a></li>
+          <li><a href="<?php echo BASE_URL; ?>/bikes">Bikes</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-100 footer-bottom">
+        <div class="avatar"></div>
+        <a href="<?php echo BASE_URL; ?>/colophon" class="text-small text-color-secondary">Colophon</a>
+        <span>&bull;</span>
+        <a href="mailto:nico@slrncl.com" class="text-small text-color-secondary">Email</a>
       </div>
     </div>
   </div>
